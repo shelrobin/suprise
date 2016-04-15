eb3eac55c2cec2094ddfa2e27aeea7294e2b43e9
shelrobin <shelbyrobin7@gmail.com>
2016-04-15 15:54:04 -0400

    got it!

--- supriseCircles.js/sketch.js
+++ supriseCircles.js/sketch.js
@@ -1,32 +1,35 @@
-function myVarX(){ 
-  if (mouseX < width && 0 <  mouseX){
-  return mouseX;
-  }else{
-  return width / 3;
-  }
-  }
+var myX = 0;
+var myY = 0;
 
-function myVarY(){ 
-  if (mouseY < height && 0 <  mouseY){
-  return mouseY;
-  }else{
-  return height / 3;
-  }
-  }
+
 function setup() {
   createCanvas(1300,1000);
 
-
+}
 function draw() {
-
+  
+if (mouseX < width && 0 <  mouseX){
+  myX = mouseX;
+  }else{
+  myX = width / 3;
   }
+  
+
+if (mouseY < height && 0 <  mouseY){
+  myY = mouseY;
+  }else{
+  myY = height / 3;
+  }
+  
+
+  
     r=random(255);
     g=random(255);
     b=random(255);
     size=random(10,140)
   fill(r,g,b)
  noStroke();
- ellipse(myVarX,myVarY,size,size);
+ ellipse(myX,myY,size,size);
 }
 
 
